2135092af73d78369c267e165d3e96a35c6de29a
alibekn6 <alibekandanuarbek@gmail.com>
2025-04-26 00:17:32 +0500

    fix: fixing 404 problem reloading

--- flashcards-client/src/App.tsx
+++ flashcards-client/src/App.tsx
@@ -1,5 +1,4 @@
-import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
-import { HashRouter as Router } from "react-router-dom";
+import { HashRouter as Router, Route, Routes, Navigate } from "react-router-dom";
 import { LoginPage } from "./pages/LoginPage";
 import { RegisterPage } from "./pages/RegisterPage";
 import { FoldersPage } from "./pages/FoldersPage";
@@ -11,22 +10,20 @@ function App() {
   return (
     <AuthProvider>
       <Router>
-        <BrowserRouter>
-          <Routes>
-            <Route path="/login" element={<LoginPage />} />
-            <Route path="/register" element={<RegisterPage />} />
-            <Route path="/folders" element={<FoldersPage />} />
-            <Route
-              path="/folders/:folderId/flashcards"
-              element={<FlashcardsPage />}
-            />
-            <Route
-              path="/folders/:folderId/flashcards/test"
-              element={<FlashcardsTestPage />}
-            />
-            <Route path="/" element={<Navigate to="/login" />} />
-          </Routes>
-        </BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/folders" element={<FoldersPage />} />
+          <Route
+            path="/folders/:folderId/flashcards"
+            element={<FlashcardsPage />}
+          />
+          <Route
+            path="/folders/:folderId/flashcards/test"
+            element={<FlashcardsTestPage />}
+          />
+          <Route path="/" element={<Navigate to="/login" />} />
+        </Routes>
       </Router>
     </AuthProvider>
   );
