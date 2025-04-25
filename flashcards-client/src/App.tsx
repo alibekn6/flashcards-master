765acf63cfe0b97cdfab0bce2ea4e5c41dc84d74
alibekn6 <alibekandanuarbek@gmail.com>
2025-04-26 00:00:54 +0500

    fix: 404 error fix

--- flashcards-client/src/App.tsx
+++ flashcards-client/src/App.tsx
@@ -1,24 +1,33 @@
-import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
-import { LoginPage } from './pages/LoginPage';
-import { RegisterPage } from './pages/RegisterPage';
-import { FoldersPage } from './pages/FoldersPage';
-import { FlashcardsPage } from './pages/FlashcardsPage';
-import { FlashcardsTestPage } from './pages/FlashcardsTestPage';
-import { AuthProvider } from './hooks/useAuth';
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
+import { LoginPage } from "./pages/LoginPage";
+import { RegisterPage } from "./pages/RegisterPage";
+import { FoldersPage } from "./pages/FoldersPage";
+import { FlashcardsPage } from "./pages/FlashcardsPage";
+import { FlashcardsTestPage } from "./pages/FlashcardsTestPage";
+import { AuthProvider } from "./hooks/useAuth";
 
 function App() {
   return (
     <AuthProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/register" element={<RegisterPage />} />
-          <Route path="/folders" element={<FoldersPage />} />
-          <Route path="/folders/:folderId/flashcards" element={<FlashcardsPage />} />
-          <Route path="/folders/:folderId/flashcards/test" element={<FlashcardsTestPage />} />
-          <Route path="/" element={<Navigate to="/login" />} />
-        </Routes>
-      </BrowserRouter>
+      <Router>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/register" element={<RegisterPage />} />
+            <Route path="/folders" element={<FoldersPage />} />
+            <Route
+              path="/folders/:folderId/flashcards"
+              element={<FlashcardsPage />}
+            />
+            <Route
+              path="/folders/:folderId/flashcards/test"
+              element={<FlashcardsTestPage />}
+            />
+            <Route path="/" element={<Navigate to="/login" />} />
+          </Routes>
+        </BrowserRouter>
+      </Router>
     </AuthProvider>
   );
 }
