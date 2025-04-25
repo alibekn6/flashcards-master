d35d12688fbefcc49570e0b06d10bf972e04cad3
alibekn6 <alibekandanuarbek@gmail.com>
2025-04-25 13:31:45 +0500

    feat: new readme file

--- flashcards-api/src/main.ts
+++ flashcards-api/src/main.ts
@@ -1,12 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
   // app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   app.useGlobalPipes(new ValidationPipe());
+
+  const config = new DocumentBuilder()
+    .setTitle('NestJS API')
+    .setDescription('Documentation of NestJS API')
+    .setVersion('0.0.1')
+    .addBearerAuth()
+    .build();
+  const options = {
+    customCss: '.swagger-ui section.models { display: none; }',
+  };
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document, options);
 
   await app.listen(3000);
 }
